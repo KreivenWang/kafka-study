d158476ae43a67dc07e54ed55aa02cbf151ed9e3
kreiven <kreiven.wang@hotmail.com>
2020-09-03 10:38:36 +0800

    add try catch

--- src/kafkaService.ts
+++ src/kafkaService.ts
@@ -31,23 +31,27 @@ class KafkaService {
   }
 
   initClient() {
-    this.producer?.close(() => console.log('producer closed'));
-    this.client?.close(() => console.log('kafka client closed'));
+    try {
+      this.producer?.close(() => console.log('producer closed'));
+      this.client?.close(() => console.log('kafka client closed'));
 
-    console.log('init Client');
-    this.client = new KafkaClient(opt.client);
-    this.client.on('brokersChanged', () => console.log(`kafka client brokersChanged`));
-    this.client.on('close', () => console.log(`kafka client closed`));
-    this.client.on('connect', () => console.log(`kafka client connect`));
-    this.client.on('error', (err) => console.error(`kafka client error`, err));
-    this.client.on('socket_error', (err) => console.error(`kafka client socket_error`, err));
-    this.client.on('ready', () => console.log(`kafka client ready`));
-    this.client.on('reconnect', () => console.log(`kafka client reconnect`));
-    this.client.on('zkReconnect', () => console.log(`kafka client zkReconnect`));
+      console.log('init Client');
+      this.client = new KafkaClient(opt.client);
+      this.client.on('brokersChanged', () => console.log(`kafka client brokersChanged`));
+      this.client.on('close', () => console.log(`kafka client closed`));
+      this.client.on('connect', () => console.log(`kafka client connect`));
+      this.client.on('error', (err) => console.error(`kafka client error`, err));
+      this.client.on('socket_error', (err) => console.error(`kafka client socket_error`, err));
+      this.client.on('ready', () => console.log(`kafka client ready`));
+      this.client.on('reconnect', () => console.log(`kafka client reconnect`));
+      this.client.on('zkReconnect', () => console.log(`kafka client zkReconnect`));
 
-    console.log('init Producer');
-    this.producer = new HighLevelProducer(this.client);
-    this.producer.on('ready', () => console.log('producer read'));
+      console.log('init Producer');
+      this.producer = new HighLevelProducer(this.client);
+      this.producer.on('ready', () => console.log('producer read'));
+    } catch (error) {
+      console.warn('init client warn', error);
+    }
   }
 
   initConsumerGroup() {
@@ -66,8 +70,11 @@ class KafkaService {
     });
     this.consumerGroup.on('error', (error) => {
       console.log(`Error while creating consumer for topic ${opt.topic}`, error);
-      console.log('reinitializing kafka client/consumer/producer');
-      this.init();
+      console.log('reinitializing kafka client/consumer/producer in 10 sec');
+      setTimeout(() => {
+        console.log('reinitializing kafka client/consumer/producer');
+        this.init();
+      }, 10000);
     });
 
     this.consumerGroup.on('message', (message) => {
@@ -82,7 +89,10 @@ class KafkaService {
     this.consumerGroup.on('rebalancing', () => console.log(`consumer group rebalancing`));
   }
 }
-
-const kfkSvc = new KafkaService();
-kfkSvc.init();
-kfkSvc.consume();
+try {
+  const kfkSvc = new KafkaService();
+  kfkSvc.init();
+  kfkSvc.consume();
+} catch (err) {
+  console.error('error catched', err);
+}
